72e2e1213c790488ed5d5f4d2d4ba67147e85738
rileysbudd <rileysbudd@gmail.com>
2024-08-09 11:16:54 -0600

    Second Push, rendering run.py irrelevant

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '<h1>Hello World!</h1>'
+
+if __name__ == "__main__":
+    app.run()
--- run.py
+++ run.py
@@ -3,5 +3,4 @@ from app import app
 if __name__ == "__main__":
     app.run()
 
-if __name__ == "__main__":
-    app.run()
+
